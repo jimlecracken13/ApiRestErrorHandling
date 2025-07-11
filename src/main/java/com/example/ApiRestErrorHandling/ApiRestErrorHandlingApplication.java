98eaf9ce87bae99069ce6b4ce8cffd73ed6fc1c9
jimlecracken14 <jimlecracken13@gmail.com>
2025-07-11 10:30:55 +0000

    first commit

---
+++ src/main/java/com/example/ApiRestErrorHandling/ApiRestErrorHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiRestErrorHandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestErrorHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestErrorHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApiRestErrorHandling/ApiRestErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiRestErrorHandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestErrorHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
